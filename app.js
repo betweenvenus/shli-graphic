67f7cc750d01905f0c07df4f4b59c892b00ea5b8
Jon Highsmith <jon@jonhighsmith.com>
2020-05-25 19:53:42 -0400

    graphic totally refactored, animations beginning to work

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const animationInit = () => {
-    const outerCircle = document.querySelector('#circle');
-    const outerWords = document.querySelector('#outer-words');
 
-    gsap.to([outerCircle, outerWords], {
-        duration: 250,
-        rotation: 360,
-        ease: Linear.easeNone,
-        transformOrigin: 'center center',
-        repeat: -1
+const animationInit = () => {
+    gsap.registerPlugin(MotionPathPlugin);
+
+    gsap.to(['#dot', '[data-name="dot"]'], {
+        motionPath: {
+            path: '#circle',
+            align: '#circle',
+            alignOrigin: [0.5, 0.5],
+            autoRotate: false,
+            relative: true
+        },
+
+        repeat: -1,
+        duration: 1,
+        ease: 'none',
+        duration: 3,
+    });
+
+    gsap.set('#bubbles', {
+        display: 'none'
     })
+
+    gsap.to('#sleeves', {
+        rotation: 360,
+        transformOrigin: 'center center',
+        repeat: -1,
+        ease: 'none',
+        duration: 15
+    });
+
+    gsap.to(['#icon', '#brain', '#icon-4'], {
+        rotation: -360,
+        transformOrigin: 'center center',
+        repeat: -1,
+        ease: 'none',
+        duration: 15
+    });
+
+    gsap.to('#gray-circle', {
+        rotation: 360,
+        ease: 'none',
+        transformOrigin: 'center center',
+        repeat: -1,
+        duration: 7
+    });
 }
 
 animationInit();
